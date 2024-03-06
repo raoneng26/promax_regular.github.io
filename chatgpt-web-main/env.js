bc0067f3116a46b2716e28dbf1fb2fe3df7c6c72
raoneng26 <146713007+raoneng26@users.noreply.github.com>
2024-03-06 18:25:21 +0800

    Add files via upload

---
+++ chatgpt-web-main/env.js
@@ -0,0 +1,4 @@
+// envAPIEndpoint="https://api.openai.com/"
+// envAPIKey="sess-1xLUQXJr3e7bzR8OYSFLI738Aa1uI94FiKURhnQB"
+envAPIEndpoint="https://free.gpt.ge"
+envAPIKey="sk-niXO1mKt0OJYL3GH1bCe4d93A73246F39eC55b36C9D6036d"
